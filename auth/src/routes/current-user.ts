3c0dfd69712ada3d315062eef502853b2788d4ec
Blake Videbeck <blake.videbeck@ezyvet.com>
2024-12-29 13:16:16 +1300

    add current user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,23 +1,10 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-	// Check if user has a JWT cookie
-	if (!req.session?.jwt) {
-		return res.send({ currentUser: null });
-	}
-
-	try {
-		// Check if JWT is valid
-		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-		// If cookie and JWT and is valid then return the info inside the JWT
-		res.send({ currentUser: payload });
-	} catch (error) {
-		// If not valid JWT then return no user
-		return res.send({ currentUser: null });
-	}
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+	res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
