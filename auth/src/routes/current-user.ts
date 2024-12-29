cfcd48a2c90b1f5d9d6bc060953a23ba0455009e
Blake Videbeck <blake.videbeck@ezyvet.com>
2024-12-29 13:00:14 +1300

    add current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+	// Check if user has a JWT cookie
+	if (!req.session?.jwt) {
+		return res.send({ currentUser: null });
+	}
+
+	try {
+		// Check if JWT is valid
+		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+		// If cookie and JWT and is valid then return the info inside the JWT
+		res.send({ currentUser: payload });
+	} catch (error) {
+		// If not valid JWT then return no user
+		return res.send({ currentUser: null });
+	}
 });
 
 export { router as currentUserRouter };
