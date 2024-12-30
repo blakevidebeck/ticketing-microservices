4adef8ed8d27d9746301c18b0bd7c9332d88545c
Blake Videbeck <blake.videbeck@ezyvet.com>
2024-12-31 10:15:03 +1300

    add get ticket by id route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import 'express-async-errors';
 import { createTicketRouter } from './routes/new';
 
 import { NotFoundError, currentUser, errorHandler } from '@bvidebecktickets/common';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,6 +17,7 @@ app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' }
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
 	throw new NotFoundError();
