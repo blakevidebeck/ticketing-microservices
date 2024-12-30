1325068f9e26e1b976eb7a8069e1cd88a68708dc
Blake Videbeck <blake.videbeck@ezyvet.com>
2024-12-30 13:26:18 +1300

    add header

---
+++ client/components/header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Link from 'next/link';
+
+const HeaderComponent = ({ currentUser }) => {
+	const links = [
+		!currentUser && { label: 'Sign up', href: '/auth/signup' },
+		!currentUser && { label: 'Sign in', href: '/auth/signin' },
+		currentUser && { label: 'Sign out', href: '/auth/signout' },
+	]
+		.filter(link => link)
+		.map(({ label, href }) => (
+			<li key={href} className='nav-item'>
+				<Link href={href}>{label}</Link>
+			</li>
+		));
+
+	return (
+		<nav className='navbar navbar-light bg-light'>
+			<Link className='navbar-brand' href='/'>
+				GitTix
+			</Link>
+
+			<div className='d-flex justify-content-end'>
+				<ul className='nav d-flex align-items-center'>{links}</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default HeaderComponent;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import HeaderComponent from '../components/header';
 
-export default ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+	return (
+		<div>
+			<HeaderComponent currentUser={currentUser} />
+			<Component {...pageProps} />
+		</div>
+	);
 };
+
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+	const client = buildClient(ctx);
+	const { data } = await client.get('/api/users/currentuser');
+
+	let pageProps = {};
+
+	if (Component.getInitialProps) {
+		pageProps = await Component.getInitialProps(ctx);
+	}
+
+	return { pageProps, ...data };
+};
+
+export default AppComponent;
