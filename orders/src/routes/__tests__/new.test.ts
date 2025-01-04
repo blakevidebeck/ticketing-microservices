ea1f12e366b7fc001d0eec36046f031b9cc0df84
Blake Videbeck <blake.videbeck@ezyvet.com>
2025-01-05 12:36:23 +1300

    implement new order tests

--- orders/src/routes/__tests__/new.test.ts
+++ orders/src/routes/__tests__/new.test.ts
@@ -1,8 +1,9 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-import { natsWrapper } from '../../nats-wrapper';
-import { Order } from '../../models/Order';
+import { Order, OrderStatus } from '../../models/Order';
+import mongoose from 'mongoose';
+import { Ticket } from '../../models/Ticket';
 
 it('has a route handler listening to /api/orders for post requests', async () => {
 	const res = await request(app).post('/api/orders').send({});
@@ -28,18 +29,75 @@ it('returns an error if an invalid ticketId is provided', async () => {
 		.expect(400);
 });
 
-it('creates a ticket with valid inputs', async () => {
+it('returns an error if the ticket does not exist', async () => {
+	const ticketId = new mongoose.Types.ObjectId().toHexString();
+
 	let orders = await Order.find({});
 	expect(orders.length).toEqual(0);
 
 	await request(app)
-		.post('/api/tickets')
+		.post('/api/orders')
 		.set('Cookie', global.signin())
-		.send({ ticketId: '' })
-		.expect(201);
+		.send({ ticketId })
+		.expect(404);
+});
 
-	orders = await Order.find({});
-	expect(orders.length).toEqual(1);
+it('returns an error if the ticket is already reserved', async () => {
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 20,
+	});
+	await ticket.save();
+
+	const order = Order.build({
+		ticket,
+		userId: 'asdf',
+		status: OrderStatus.Created,
+		expiresAt: new Date(),
+	});
+	await order.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId: ticket.id })
+		.expect(400);
+});
+
+it('creates an order if existing order status was cancelled', async () => {
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 20,
+	});
+	await ticket.save();
+
+	const order = Order.build({
+		ticket,
+		userId: 'asdf',
+		status: OrderStatus.Cancelled,
+		expiresAt: new Date(),
+	});
+	await order.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId: ticket.id })
+		.expect(201);
+});
+
+it('creates an order if no existing order', async () => {
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 20,
+	});
+	await ticket.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId: ticket.id })
+		.expect(201);
 });
 
 // it('publishes a create event', async () => {
