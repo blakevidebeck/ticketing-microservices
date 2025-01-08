a803ae0b952f950cfbb251766fae2f25e7c893ae
Blake Videbeck <blake.videbeck@ezyvet.com>
2025-01-08 19:30:14 +1300

    add expiration listener and queue

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,18 @@
+import { Listener, NotFoundError, OrderCreatedEvent, Subjects } from '@bvidebecktickets/common';
+import { Message } from 'node-nats-streaming';
+import { QueueGroupNames } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+	readonly subject = Subjects.OrderCreated;
+	readonly queueGroupName = QueueGroupNames.EXPIRATION_SERVICE;
+
+	onMessage = async (data: OrderCreatedEvent['data'], message: Message) => {
+		const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+		console.log('waiting this many ms to process the job', delay);
+
+		await expirationQueue.add({ orderId: data.id }, { delay });
+
+		message.ack();
+	};
+}
---
+++ expiration/src/events/listeners/queue-group-name.ts
@@ -0,0 +1,3 @@
+export enum QueueGroupNames {
+	EXPIRATION_SERVICE = 'expiration-service',
+}
