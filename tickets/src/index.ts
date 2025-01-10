6a0d521ca5167a4db8f9985f64684888c7c57791
Blake Videbeck <blake.videbeck@ezyvet.com>
2025-01-11 12:15:34 +1300

    add another log

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,6 +6,8 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
+	console.log('Starting up...');
+
 	// Handle no JWT_KEY
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
